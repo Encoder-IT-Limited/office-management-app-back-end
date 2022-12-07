ac2d0ceda626a6499bd83b50f2113477a7bcfcec
AR Ridoy <ridoy@encoderit.net>
2022-12-07 15:46:40 +0600

    Squashed commit of the following:
    commit 43ffe89d596abe673e66a440297f212d7511e9b8
    Author: AR Ridoy <ridoy@encoderit.net>
    Date:   Wed Dec 7 15:25:08 2022 +0600
    
        initial migration completed

---
+++ database/migrations/2022_12_07_080815_create_leave_management_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveManagementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_management', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status');
+            $table->string('reason');
+            $table->date('accepted_start_date');
+            $table->date('accepted_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_management');
+    }
+}
