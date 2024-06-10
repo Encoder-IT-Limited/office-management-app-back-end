fc1b31c0b09985898cda81d0b8c4a44140fdb924
encoder <nahid@encoderit.net>
2024-06-10 13:06:25 +0600

    Billable time Role check

--- app/Http/Controllers/BillableTimeController.php
+++ app/Http/Controllers/BillableTimeController.php
@@ -38,11 +38,17 @@ class BillableTimeController extends Controller
         if (request('ids')) {
             $billableTime->whereIn('id', request('ids'));
         }
-        if (request('by_user')) {
-            $billableTime->whereIn('user_id', request('by_user'));
-        }
-        if (request('by_project')) {
-            $billableTime->whereIn('project_id', request('by_project'));
+        if (auth()->user()->hasRole('admin')) {
+            if (request('by_user')) {
+                $billableTime->whereIn('user_id', request('by_user'));
+            }
+            if (request('by_project')) {
+                $billableTime->whereIn('project_id', request('by_project'));
+            }
+        } else if (auth()->user()->hasRole('developer') && !empty(auth()->user()->children)) {
+            $billableTime->whereIn('user_id', auth()->user()->children->pluck('id'));
+        } else {
+            $billableTime->where('user_id', auth()->id());
         }
         if (request('start_date')) {
             $billableTime->where('date', '>=', request('start_date'));
