cbf91ca43589edb13945db744764710f1a10c288
nahid <nahidferdous240@gmail.com>
2024-07-05 06:16:11 +0600

    !

---
+++ database/migrations/2024_04_15_121858_create_billable_times_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillableTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billable_times', function (Blueprint $table) {
+            $table->id();
+            $table->string('task')->nullable();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('task_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('site')->nullable();
+            $table->dateTime('date')->nullable();
+            $table->string('time_spent')->nullable();
+            $table->longText('comment')->nullable();
+            $table->longText('screenshot')->nullable();
+            $table->string('given_time')->nullable();
+            $table->boolean('is_freelancer')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billable_times');
+    }
+}
