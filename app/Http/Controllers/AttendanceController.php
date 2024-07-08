d485f9558272de7ca7ca1174bc5a238b8cfe8852
encoder <nahid@encoderit.net>
2024-07-08 14:17:21 +0600

    project status

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -150,7 +150,6 @@ class AttendanceController extends Controller
                 $dateQ->whereDay('check_in', '=', $request->date);
             });
         } else if ($user->hasRole('developer')) {
-            dd('nahid');
             $queries = Attendance::with('employee')
                 ->where('employee_id', $user->id)
                 ->when($request->has('date'), function ($dateQ) use ($request) {
