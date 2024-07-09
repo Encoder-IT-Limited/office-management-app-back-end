6fb55b14e7df7753a6693d391d4350a34335c170
nahid <nahidferdous240@gmail.com>
2024-07-09 15:50:02 +0600

    Project with reminders

--- app/Http/Requests/ReminderStoreRequest.php
+++ app/Http/Requests/ReminderStoreRequest.php
@@ -28,7 +28,7 @@ class ReminderStoreRequest extends FormRequest
             'project_id' => 'required|exists:projects,id',
             'title' => 'required|string',
             'description' => 'required|string',
-            'remind_at' => 'required|date:Y/m/d|after:now',
+            'remind_at' => 'required|date|after:now',
             'message' => 'sometimes|required|boolean',
         ];
     }
