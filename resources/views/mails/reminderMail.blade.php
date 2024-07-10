d6bca88caa0b2e6eeaf0aecd554f48c30d90da1f
encoder <nahid@encoderit.net>
2024-07-10 15:01:26 +0600

    Schedule added

--- resources/views/mails/reminderMail.blade.php
+++ resources/views/mails/reminderMail.blade.php
@@ -1,13 +1,11 @@
-{{$reminder}}
+<h2>Reminder Email</h2>
 
-{{--<h2>Reminder Email</h2>--}}
-
-{{--#Dear {{ $reminder->users->name }},--}}
-{{--<p>--}}
-{{--    You have a reminder for project "<p>{{$reminder->project->name}}</p>"--}}
-{{--on {{ date('d-m-Y', strtotime($reminder->remind_at)) }}--}}
-{{--at {{ date('h:i a', strtotime($reminder->remind_at)) }}--}}
-{{--with {{ $reminder->clients->name }}--}}
-{{--</p>--}}
-{{--<h4>Title: {{$reminder->title}}</h4>--}}
-{{--<p>{{$reminder->description}}</p>--}}
+#Dear {{ $reminder->users->name }},
+<p>
+    You have a reminder for project "<p>{{$reminder->project->name}}</p>"
+on {{ date('d-m-Y', strtotime($reminder->remind_at)) }}
+at {{ date('h:i a', strtotime($reminder->remind_at)) }}
+with {{ $reminder->clients->name }}
+</p>
+<h4>Title: {{$reminder->title}}</h4>
+<p>{{$reminder->description}}</p>
