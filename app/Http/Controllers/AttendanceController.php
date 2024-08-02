7221d9e75b41e18cd4f154d723c43c617ba7cc8e
nahid <nahidferdous240@gmail.com>
2024-08-02 15:02:20 +0600

    Attendance Default Delay Time issue

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -201,7 +201,7 @@ class AttendanceController extends Controller
             $employees = User::withCount(['attendances AS delay_count' => function ($attendance) {
                 $attendance->whereYear('check_in', '=', $this->year)
                     ->whereMonth('check_in', '=', $this->month)
-                    ->whereNotNull('check_in');
+                    ->whereNotNull('delay_time');
             }])->onlyDeveloper()
                 ->having('delay_count', '>', 0) // Add this line to filter users with non-zero delay count
                 ->latest()
